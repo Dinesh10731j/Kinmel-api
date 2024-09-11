da5d9add391799345f9c570e706784925f3d4ba2
Dinesh10731j <dinesh1122@gmail.com>
2024-09-11 11:40:59 +0545

    name filed  field is added in users model

---
+++ Model/cart.model.js
@@ -0,0 +1,15 @@
+// import mongoose from "mongoose";
+
+// const CartSchema = new mongoose.Schema({
+//     userId:{
+//         type:mongoose.Schema.ObjectId
+//     },
+
+//     itemId:[],
+// });
+
+
+// const CartModel = mongoose.model('Cart',CartSchema);
+
+
+// export default CartModel;
