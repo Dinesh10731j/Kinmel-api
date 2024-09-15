8d89b2c9fcaa3ba042cef104e27c0a751ff571b9
Dinesh10731j <dinesh1122@gmail.com>
2024-09-15 17:40:53 +0545

    minor changes in addressbook model schema

--- Model/addressbook.model.js
+++ Model/addressbook.model.js
@@ -4,6 +4,7 @@ const addressBookSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.ObjectId,
     ref: "User",
+    required:[true,'userId is required']
   },
   name: {
     type: String,
