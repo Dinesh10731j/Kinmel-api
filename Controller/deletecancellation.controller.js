3da4b963223bbc7fc66dd8e504c20d29f7af40bd
Dinesh10731j <dinesh1122@gmail.com>
2024-09-22 14:44:09 +0545

    delete cancellation endpoint completed

---
+++ Controller/deletecancellation.controller.js
@@ -0,0 +1,32 @@
+import cancellationModel from "../Model/cancellation.model.js";
+const deleteCancellation = async (req, res) => {
+  try {
+    const { cancellationId } = req.params;
+
+    const deletedCancellation = await cancellationModel.findByIdAndDelete(
+      cancellationId
+    );
+
+    if (!deletedCancellation) {
+      return res
+        .status(400)
+        .json({ msg: "Failed to delete cancellation", success: false });
+    }
+
+    res
+      .status(200)
+      .json({ msg: "Cancellation deleted succesfully", sucecss: true });
+  } catch (err) {
+    res
+      .status(500)
+      .josn({
+        msg: "Internal srver error",
+        success: false,
+        error: err.message,
+      });
+  }
+};
+
+
+
+export default deleteCancellation;
