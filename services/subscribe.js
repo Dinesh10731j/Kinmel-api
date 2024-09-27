f2b580686e3e2c98329bb2ee13851754296d401d
Dinesh10731j <dinesh1122@gmail.com>
2024-09-27 12:52:34 +0545

    ]integrate Nodemailer for subscription confirmation emails

--- services/subscribe.js
+++ services/subscribe.js
@@ -1,1 +1,37 @@
 import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+
+const transporter = nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:process.env.GMAIL_USER,
+        pass:process.env.GMAIL_PASS,
+    }
+});
+
+
+
+export const sendSubscriptionMessage = (to,subject)=>{
+    const text = 'Thank you for subscribing KinMel';
+
+    const mailOptions = {
+        to :to,
+        subject:subject,
+        text:text,
+
+    };
+    transporter.sendMail(mailOptions,(err,info)=>{
+        if(err){
+            console.log('Error while sending email',err.message);
+        }else{
+            console.log('Email send successfully',info.messageId);
+        }
+    })
+
+    
+
+}
+
+
+
+export default sendSubscriptionMessage();
