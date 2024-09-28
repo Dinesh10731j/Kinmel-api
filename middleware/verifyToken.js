ac1e8d0c9d77e100e4d1bd40672ba6673c27068c
Dinesh10731j <dinesh1122@gmail.com>
2024-09-28 19:11:54 +0545

    minor changes done

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const verifyJwtToken = (roleRequired) => {
     return (req, res, next) => {
-        // Extract token from the Authorization header
+        
         const token = req.headers['authorization']?.replace('Bearer ', '').trim();
 
         if (!token) {
@@ -12,20 +12,20 @@ const verifyJwtToken = (roleRequired) => {
         }
 
         try {
-            // Verify the token using the secret key
+            
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            console.log(decoded);
-            req.user = decoded; // Assign the decoded token to req.user
+          
+            req.user = decoded; 
 
-            // Role-based access control
+           
             if (req.user.admin !== roleRequired) {
                 return res.status(403).json({ 
-                    msg: `Access denied. Only ${roleRequired}s are allowed.`, 
+                    msg: `Access denied. Only ${roleRequired} are allowed.`, 
                     success: false 
                 });
             }
 
-            // Continue to the next middleware or route handler
+          
             next();
         } catch (error) {
             return res.status(401).json({ msg: 'Invalid token', success: false });
